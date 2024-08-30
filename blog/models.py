33be9a54626f97897f5321673fe29af54659ae23
Costa <lfelipecbarreto@gmail.com>
2024-08-30 20:39:47 -0300

    finalizacao

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Interesses(models.Model):
     nome = models.CharField(max_length=100)
-    descricao = models.TimeField()
+    descricao = models.TextField()
     nivel = models.FloatField()
     def __str__(self):
         return self.nome
